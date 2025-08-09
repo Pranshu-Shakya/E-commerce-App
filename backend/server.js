030b05c5b91451887cc2fcb4c76c5285b0fd9ba9
Pranshu Shakya <pranshushakya2027@gmail.com>
2025-08-09 14:59:51 +0530

    cors error fixed

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ connectCloudinary();
 //middlewares
 app.use(
 	cors({
-		origin: "http://localhost:5173",
+		origin: ["http://localhost:5173", "https://forever-app-frontend.vercel.app"],
 		credentials: true,
 	})
 );
