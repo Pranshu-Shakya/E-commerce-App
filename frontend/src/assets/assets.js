9788f60c4318b7f8e6de55690c2bc8a63b2d1c9b
Pranshu Shakya <pranshushakya2027@gmail.com>
2025-11-25 12:56:37 +0530

    hero section

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -57,7 +57,7 @@ import profile from './profile.png'
 
 
 import logo from './logo.png'
-import hero_img from './hero_img.png'
+import hero_img from './p_img2_2.png'
 import cart_icon from './cart_icon.png'
 import bin_icon from './bin_icon.png'
 import dropdown_icon from './dropdown_icon.png'
@@ -729,5 +729,6 @@ export const products = [
         date: 1716668445448,
         bestseller: false
     }
+
 
 ]
